19c5ab78f7da82e54cdaece04f5d9328a68f5605
清影 <15186828064@163.com>
2021-06-15 23:56:31 +0800

    自定义的日志格式不好用。。
    自定义的日志格式不好用。。

--- middleware/log.go
+++ middleware/log.go
@@ -1,24 +1,24 @@
 package middleware
-
-import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-)
-
-// CostumerLog 自定义日志输出格式
-func CostumerLog() gin.HandlerFunc {
-	// LoggerWithFormatter 中间件会将日志写入 gin.DefaultWriter
-	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-		return fmt.Sprintf("%s - %s \"%s %s %s %d %s \"%s\" %s\"\n",
-			param.TimeStamp.Format("2006-01-02 03:04:05"),
-			param.ClientIP,
-			param.Method,
-			param.Path,
-			param.Request.Proto,
-			param.StatusCode,
-			param.Latency,
-			param.Request.UserAgent(),
-			param.ErrorMessage,
-		)
-	})
-}
+//
+//import (
+//	"fmt"
+//	"github.com/gin-gonic/gin"
+//)
+//
+//// CostumerLog 自定义日志输出格式
+//func CostumerLog() gin.HandlerFunc {
+//	// LoggerWithFormatter 中间件会将日志写入 gin.DefaultWriter
+//	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+//		return fmt.Sprintf("%s |\t%s |\t%d |\t%s |\t\"%s\" |\t%s |\t%s |\t%s |\t%s \n",
+//			param.TimeStamp.Format("2006-01-02 03:04:05"),
+//			param.ClientIP,
+//			param.StatusCode,
+//			param.Method,
+//			param.Path,
+//			param.Request.Proto,
+//			param.Latency,
+//			param.Request.UserAgent(),
+//			param.ErrorMessage,
+//		)
+//	})
+//}
