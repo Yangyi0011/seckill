3ed094e9233f0a9c1491975d073b26562f08228d
清影 <15186828064@163.com>
2021-06-15 23:58:44 +0800

    收集 gin 日志到指定文件
    收集 gin 日志到指定文件

--- main.go
+++ main.go
@@ -9,19 +9,21 @@ import (
 	"seckill/router"
 )
 
-func main() {
+func init() {
 	// 收集 http 请求日志
 	logfile, err := os.Create("./gin_http.log")
 	if err != nil {
 		fmt.Println("Could not create log file")
 	}
-	// 启用发布模式
-	gin.SetMode(gin.ReleaseMode)
 	// 指定默认的日志输出路径
 	gin.DefaultWriter = io.MultiWriter(logfile)
+}
 
+func main() {
+	// 启用发布模式
+	gin.SetMode(gin.DebugMode)
 	// 启动
-	err = router.InitRouter().Run()
+	err := router.InitRouter().Run()
 	if err != nil {
 		log.Fatal("项目启动错误：", err)
 		return
